cca56856219b66f9d6785fe49c4d4aac6fffbb12
nirch <nirh2@yahoo.com>
2023-04-13 08:44:27 +0300

    no env var

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const server = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URI
-    ? process.env.REACT_APP_SERVER_URI
-    : 'http://localhost:3000',
+  baseURL: 'http://localhost:3000',
 });
 
 function fetchPosts() {
